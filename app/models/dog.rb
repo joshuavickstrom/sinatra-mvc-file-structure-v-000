7f60253a081f994e25c9aad09795be7f288aa737
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-27 16:48:00 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,15 +1,15 @@
 class Dog
 
-attr_accessor :name, :breed, :age
+  attr_accessor :name, :breed, :age
 
-def initialize(name, breed, age)
-  @name = name
-  @breed = breed
-  @age = age
-end
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
-def all
+  def all
 
-end
+  end
 
 end
